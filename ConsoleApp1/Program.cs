b98b2385b04b399b811432531e0e6433eb765885
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2018-12-12 08:39:42 +0100

    abbiamo creto una soluzione in cui si inizializza una risultato in stringa e un numero gia fisso cioè 255

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string risultato;
+            int num = 255;
+            risultato = "";
+            while (num < 1)
+            {
+                int resto = num % 2;
+                risultato = Convert.ToString(resto) + risultato;
+                num = num / 2;
+            }
+            risultato = Convert.ToString(num) + risultato;
+            Console.WriteLine($"il numero binario è {risultato}");
+            Console.ReadLine();
+        }
+    }
+}
